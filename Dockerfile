743a331ef4fe0609b9605eca21afcdc3dba57133
theerudito <erudito.tv@gmail.com>
2025-03-31 20:51:21 -0500

    v1

--- Dockerfile
+++ Dockerfile
@@ -7,5 +7,5 @@ RUN dotnet publish -c Release -o /app/out
 FROM mcr.microsoft.com/dotnet/aspnet:9.0 AS runtime
 WORKDIR /app
 COPY --from=build /app/out .
-EXPOSE 8081
+EXPOSE 8080
 ENTRYPOINT ["dotnet", "Bingo.dll"]
--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddScoped<IEmail, EmailService>();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
